12fcc081cf75c4221ac65ffa86c6d927ac51a5c9
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2021-01-10 21:26:13 -0800

    Install Laravel Fortify

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,17 @@
+@prop([
+    'method' => 'POST',
+    'action' => '',
+])
+
+<form
+    method="{{ $method === 'GET' ? 'GET' : 'POST' }}"
+    action="{{ $action }}"
+    {{ $attributes }}>
+    @csrf
+
+    @if (! in_array($method, ['GET', 'POST']))
+        @method($method)
+    @endif
+
+    {{ $slot }}
+</form>
